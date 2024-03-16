99f59f26b28dc273ddd0bf32e7b536636a126b9c
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-03-16 20:02:01 +0530

    feat : interviewer

--- extraction.py
+++ extraction.py
@@ -6,7 +6,7 @@ from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 
 # vector embedding
-from embed import retriever
+# from embed import retriever
 from dataclasses import dataclass
 from typing import Literal
 
@@ -17,44 +17,41 @@ class Message:
 
 information = {'stack': ['python', 'javascript'], 'experience_in_years': '2'}
 
-class templates: 
-
-    behavioral_template = """ I want you to act as an interviewer. Remember, you are the interviewer not the candidate.   
-            Let's think step by step.
-            
-            Based on the keywords, 
-            Create a guideline with the following topics for a behavioral interview to test the soft skills of the candidate. 
-            
-            Do not ask the same question.
-            Do not repeat the question. 
-            
-            Keywords: 
-            {context}
-
-            Question: {question}
-            Answer:"""
-    
-    conversation_template = """I want you to act as an interviewer strictly following the below guidelines in the current conversation.
-                            Candidate has no idea what the guideline is.
-                            Ask me questions and wait for my answers. Do not write explanations.
-                            Ask each question like a real person, only one question at a time.
-                            Do not ask the same question.
-                            Do not repeat the question.
-                            Do ask follow-up questions if necessary. 
-                            Your name is MistralInterviewer.
-                            I want you to only reply as an interviewer.
-                            Do not write all the conversation at once.
-                            If there is an error, point it out.
-
-                            Current Conversation:
-                            {history}
-
-                            Candidate: {input}
-                            AI: """
-
-Behavioral_Prompt = PromptTemplate(input_variables=["context", "question"],template=templates.behavioral_template)
 
 history = []
+history.append(Message("ai", """I want you to act as an interviewer strictly following the below guidelines in the current conversation.
+Ask questions and wait for my answers. Do not write explanations.
+Ask each question like a real person, only one question at a time.
+Do not ask the same question.
+Do not repeat the question.
+Your name is MistralInterviewer.
+Do not write all the conversation at once.
+"""))
 history.append(Message("ai", "Hello there! I am your interviewer today. I will access your soft skills through a series of questions. Let's get started! Please start by saying hello or introducing yourself."))
 
-memory = ConversationBufferMemory()
+# memory = ConversationBufferMemory()
+
+def conversation_template(history):
+        return f"""
+        Current Conversation:
+        {history}
+        Do ask follow-up questions if necessary.If there is an error, point it out.
+        """
+
+
+while(1):
+
+        PROMPT = conversation_template(history)
+
+        question = queryMistral(PROMPT)
+        print(question)
+
+        answer = input("\nAnswer : ")
+
+        # memory.save_context({"input": question}, {"output": answer})
+        history.append(Message(origin="ai", message=question))
+        history.append(Message(origin="human", message=answer))
+
+        print("--------------------\n\n")
+        print(history)
+        print("--------------------\n\n")
--- mistral.py
+++ mistral.py
@@ -12,7 +12,12 @@ headers = {"Authorization": f"Bearer {HF_BEARER_TOKEN}"}
 def queryMistral(query : str):
     response = requests.post(API_URL, headers=headers, json={"inputs":f"<s>[INST] {query} [/INST]"})
     res = response.json()
-    return res[0]['generated_text']
+    res = postProcessMistralOutput(res[0]['generated_text'])
+    return res
 
 def postProcessMistralOutput(input : str):
-    pass
+    inst_index = input.find("[/INST]")
+    if inst_index != -1:
+        return input[inst_index + len("[/INST]"):].strip()
+    else:
+        return input
